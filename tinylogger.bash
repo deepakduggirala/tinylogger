b66e1a2905f9cfaabd04c09ab983c3a50e5b6c6b
Deepak Duggirala <deepak.duggirala@target.com>
2019-01-30 15:56:01 +0530

    add logger tags

--- tinylogger.bash
+++ tinylogger.bash
@@ -28,11 +28,11 @@ LOGGER_FMT=${LOGGER_FMT:="%Y-%m-%d %H:%M:%S"}
 LOGGER_LVL=${LOGGER_LVL:="info"}
 
 function tlog {
-    action=$1 && shift
-    case $action in 
-        debug)  [[ $LOGGER_LVL =~ debug ]]           && echo "$( date "+${LOGGER_FMT}" ) - DEBUG - $@" 1>&2 ;;
-        info)   [[ $LOGGER_LVL =~ debug|info ]]      && echo "$( date "+${LOGGER_FMT}" ) - INFO - $@" 1>&2  ;;
-        warn)   [[ $LOGGER_LVL =~ debug|info|warn ]] && echo "$( date "+${LOGGER_FMT}" ) - WARN - $@" 1>&2  ;;
-        error)  [[ ! $LOGGER_LVL =~ none ]]          && echo "$( date "+${LOGGER_FMT}" ) - ERROR - $@" 1>&2 ;;
-    esac
-    true; }
+  action=$1 && shift
+  case $action in
+    debug)  [[ $LOGGER_LVL =~ debug ]]           && echo "$( date "+${LOGGER_FMT}" ) - DEBUG - $LOGGER_TAGS $@" 1>&2 ;;
+    info)   [[ $LOGGER_LVL =~ debug|info ]]      && echo "$( date "+${LOGGER_FMT}" ) - INFO - $LOGGER_TAGS $@" 1>&2  ;;
+    warn)   [[ $LOGGER_LVL =~ debug|info|warn ]] && echo "$( date "+${LOGGER_FMT}" ) - WARN - $LOGGER_TAGS $@" 1>&2  ;;
+    error)  [[ ! $LOGGER_LVL =~ none ]]          && echo "$( date "+${LOGGER_FMT}" ) - ERROR - $LOGGER_TAGS $@" 1>&2 ;;
+  esac
+true; }
